4a3a299daa79dbc37563eb381cfc20f0ad675ef7
JuanGalilea <juangk93@gmail.com>
2026-06-23 17:26:48 +0200

    feat(lefthook): remove husky as hook (#7)
    * feat(lefthook): remove husky as hook
    
    * remove husky folder
    
    * prepare fail fix
    
    * validate schemas pre-commit
    
    * readme update

--- src/push-data.ts
+++ src/push-data.ts
@@ -2,7 +2,6 @@ import type { Actor } from 'apify';
 
 export const pushDataCountToDataset = async (options: { count: number; actor: typeof Actor }) => {
     const { count, actor } = options;
-
     const data = Array.from({ length: count }, (_, i) => ({ id: i }));
 
     await actor.pushData(data);
